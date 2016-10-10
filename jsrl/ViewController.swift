5e42f2f9a60392e496916a5b071b210b98387348
Fiskie <myself@fiskie.me>
2016-10-10 15:06:34 +0100

    Initial Commit

---
+++ jsrl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsrl
+//
+//  Created by Fisk on 10/10/2016.
+//  Copyright © 2016 fisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
